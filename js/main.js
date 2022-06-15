9608e3077d0ff609581829e010b8c9e263bc300b
okasyun <shunshun1207shun@keio.jp>
2022-06-15 17:42:55 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// const btn = document.querySelector(".mobile-menu__btn");
+// const header_current = document.querySelector(".header__li");
+// const mobile_current = document.querySelector(".mobile-menu__item");
+
+// btn.addEventListener("click", () => {
+//     mobile_menu.classList.toggle('menu-open');
+// });
+
+// header_current.addEventListener("click", () => header_current.classList.toggle('current'));
+// mobile_current.addEventListener("click", () => mobile_current.classList.toggle('current'));
+
+
+$(".mobile-menu__btn").click(function () {
+    $(this).toggleClass('menu-open');
+    $('.mobile-menu').toggleClass('menu-open');
+});
+
+$(".mobile-menu a").click(function () {
+    $('mobile-menu').removeClass('menu-open');
+    $('mobile-menu__btn').removeClass('menu-open');
+});
+
+$(".header__li").click(function () {
+    $(this).toggleClass('current');
+});
+
+// クリックしたら今までのcurrentを削除するようにできていない
+$(".mobile-menu__item").click(function () {
+    $(this).toggleClass('current');
+});
