159b3266620d2cb8751bb7fdec4e0bfc6ef43fba
okasyun <shunshun1207shun@keio.jp>
2022-06-24 18:13:44 +0900

    update gallery.html

--- scripts/home.js
+++ scripts/home.js
@@ -8,16 +8,25 @@
 // header_current.addEventListener("click", () => header_current.classList.toggle('current'));
 // mobile_current.addEventListener("click", () => mobile_current.classList.toggle('current'));
 
+
+titles = ['Akihabara','Asakusa','Ueno','Nakano', 'Shinjuku', 'Sugamo', 'Akabane', 'Akihabara', 'Ochanomizu', 'Sapporo', 'Tateishi', 'Nakano', 'Shinjuku', 'Otaru', 'Akihabara', 'Kitasenju', 'Sapporo', 'Asakusa', 'Shibuya', 'Shinjuku', 'Shibuya', 'Akihabara', 'Sapporo', 'Sapporo', 'Ootemachi', 'Tateishi', 'Koenji', 'Asakusa', 'Sapporo', 'Ootemachi', 'Kokudo', 'Yokohama', 'Shinjuku', 'Ootemachi', 'Shinbashi', 'Yokohama', 'Shinbashi', 'Ootemachi', 'Shinjuku', 'Shinbashi', 'Shinjuku', 'Yokohama', 'Ueno', 'Tateishi', 'Shinbashi', 'Nakano', 'Shinjuku', 'Shinbashi', 'Shinjuku', 'Asakusa', 'Koenji', 'Shinjuku', 'Shinjuku', 'Sapporo', 'Ootemachi', 'Yokohama', 'Yokohama', 'Uguisudani', 'Tateishi', 'Roppongi', 'Noge', 'Shinjuku', 'Shinjuku', 'Yokohama', 'Musashiseki', 'Sapporo', 'Akabane'];
 window.onload = function () {
     const images = document.querySelectorAll("img")
-    let num = 0;
+    const title = document.querySelector(".title");
+    if (images.length=!titles.length){
+        console.log('エラー');
+    }
     console.log(images);
-
+    let num = 0;
+    console.log(images.length);
+    console.log(titles.length);
     const element = document.querySelector(".img-display__show");
     element.addEventListener('animationiteration', slide_animate);
+    title.innerHTML = titles[num];
 
     function slide_animate() {
-        num++;
+        num++
+        title.innerHTML = titles[num];
         const image = document.querySelector("div .active")
         image.classList.remove("active")
         if (num >= images.length) {
